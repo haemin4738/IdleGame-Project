9e0d211a645258646ec5a42e9baeb64baa7ff6c9
haemin4738 <leeheamin12@gmail.com>
2026-08-28 17:05:18 +0900

    ✨ feat: EXP 게이지 바 추가

--- Assets/Scripts/UI/HUDView.cs
+++ Assets/Scripts/UI/HUDView.cs
@@ -1,11 +1,13 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUDView : MonoBehaviour
 {
     [SerializeField] TMP_Text goldText;
     [SerializeField] TMP_Text levelText;
     [SerializeField] TMP_Text expText;
+    [SerializeField] Slider expBar;
 
     void OnEnable()
     {
@@ -27,10 +29,16 @@ public class HUDView : MonoBehaviour
         var data = SaveManager.Instance.Data;
         goldText.text = $"골드: {data.gold:N0}";
         levelText.text = $"Lv.{data.playerLevel}";
-        expText.text = $"EXP: {data.playerExp} / {GameManager.ExpRequired(data.playerLevel)}";
+        long required = GameManager.ExpRequired(data.playerLevel);
+        expText.text = $"EXP: {data.playerExp} / {required}";
+        if (expBar != null) expBar.value = (float)data.playerExp / required;
     }
 
     void OnGoldChanged(GoldChangedEvent e)    => goldText.text = $"골드: {e.NewAmount:N0}";
     void OnLevelUp(LevelUpEvent e)            => levelText.text = $"Lv.{e.NewLevel}";
-    void OnExpChanged(PlayerExpChangedEvent e) => expText.text = $"EXP: {e.CurrentExp} / {e.RequiredExp}";
+    void OnExpChanged(PlayerExpChangedEvent e)
+    {
+        expText.text = $"EXP: {e.CurrentExp} / {e.RequiredExp}";
+        if (expBar != null) expBar.value = (float)e.CurrentExp / e.RequiredExp;
+    }
 }
